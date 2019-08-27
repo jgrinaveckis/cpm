5736eb6a8a7549de7653de951df539d607355a42
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2019-08-27 21:56:00 +0300

    mazi pakeitimai ir TODO commentas

--- tasks_durations.py
+++ tasks_durations.py
@@ -8,18 +8,19 @@ def adding_tasks_dates(nodeslist: list, date: datetime.datetime):
     end = datetime.time(17)
     for node in nodeslist[::-1]:
         dur = node[1]
-        #while (dur => 0)?
-        if (start <= (cur_date - datetime.timedelta(hours=dur)).time() <= end):
-            nodes_durations.extend([((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),\
-            cur_date.strftime('%Y-%m-%d %H:%M'))])
-            cur_date = cur_date - datetime.timedelta(hours=node[1])
-        else:
+        while dur >= 0:
+            #TODO: tikrinti prasisukima jei didelis uzduoties atlikimo laikas ir dur skaiciuoti
+            if (start <= (cur_date - datetime.timedelta(hours=dur)).time() <= end):
+                nodes_durations.extend([((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),\
+                cur_date.strftime('%Y-%m-%d %H:%M'))])
+                cur_date = cur_date - datetime.timedelta(hours=node[1])
+            else:
 
-            cur_date = cur_date-datetime.timedelta(days=1)
-            cur_date = cur_date.replace(hour=17)
-            nodes_durations.extend([((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),\
-            cur_date.strftime('%Y-%m-%d %H:%M'))])
-            cur_date = cur_date - datetime.timedelta(hours=node[1])
+                cur_date = cur_date-datetime.timedelta(days=1)
+                cur_date = cur_date.replace(hour=17)
+                nodes_durations.extend([((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),\
+                cur_date.strftime('%Y-%m-%d %H:%M'))])
+                cur_date = cur_date - datetime.timedelta(hours=node[1])
     #dict kur key - taskas, value - pradzia ir pabaiga
     info_dict = dict(zip(nodes,nodes_durations[::-1]))
     return info_dict
