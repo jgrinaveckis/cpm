2a20f57f185f7170283aaab245cc09a99e617201
jgrinaveckis <grinaveckisjustas@gmail.com>
2019-08-27 12:25:48 +0300

    Pridetas ganto grafiko braizymas

--- cpm.py
+++ cpm.py
@@ -1,6 +1,7 @@
 import networkx as nx
-from datetime import date
+import plotly.figure_factory as ff
 import datetime
+
 
 class CPM(nx.DiGraph):
 
@@ -118,18 +119,40 @@ def addingdurations(sortedNodesList, originalNodesList):
     testList = sorted(originalNodesList, key=lambda x: sortedNodesList.index(x[0]))
     return testList
 
-def adding_tasks_dates(nodeslist, date):
+def adding_tasks_dates(nodeslist: list, date: datetime.datetime):
     nodes = [i[0] for i in nodeslist]
     nodes_durations = []
     cur_date = date
     for node in nodeslist[::-1]:
-        nodes_durations.append((cur_date.strftime('%m/%d/%Y, %H:%M'),(cur_date - datetime.timedelta(hours=node[1])).strftime('%m/%d/%Y, %H:%M')))
+        nodes_durations.append(((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),cur_date.strftime('%Y-%m-%d %H:%M')))
         cur_date = cur_date - datetime.timedelta(hours=node[1])
-
-#dict kur key - taskas, value - pradzia ir pabaiga
+    #dict kur key - taskas, value - pradzia ir pabaiga
     info_dict = dict(zip(nodes,nodes_durations[::-1]))
-    print(info_dict)
-   # print(nodes_durations[::-1])
+    #print(info_dict)
+    return info_dict
+
+def build_tasks_dict(tasksdatesdict:dict):
+    nodes_names = tasksdatesdict.keys()
+    operations = []
+    new_dict = {}
+    for x in nodes_names:
+        operations.append(x.split('-')[1])
+    operations = list(set(operations))
+    for name in operations:
+        new_dict[name] = {}
+        for k, v in tasksdatesdict.items():
+            if name in k.split('-')[1]:
+                new_dict[name][k] = v
+    #print(new_dict)
+    return new_dict
+    
+def plotting_gantt(tasksdict:dict):
+    df = []
+    for out_k, out_v in tasksdict.items():
+        for in_k, in_v in out_v.items():
+            df.append(dict(Task=out_k,Start=in_v[0], Finish=in_v[1],Resource=in_k))
+    fig = ff.create_gantt(df, index_col='Task', show_colorbar=True, group_tasks=True, showgrid_x=True, showgrid_y=True, data='Resource', bar_width=0.5) 
+    fig.show()
 
 
     
@@ -187,6 +210,8 @@ if __name__ == "__main__":
     G4.critical_path_length
     kl_list = sortingnodes(G4.nodes.data('KL'))
     testlist = addingdurations(kl_list,list(G4.nodes.data('duration')))
-    adding_tasks_dates(testlist, datetime.datetime(2019,8,26,23,00))
+    nodesdict = adding_tasks_dates(testlist, datetime.datetime(2019,8,26,23,00))
+    tasks_durations = build_tasks_dict(nodesdict)
+    plotting_gantt(tasks_durations)
     #print(G4.nodes.data())
 
