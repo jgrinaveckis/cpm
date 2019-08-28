6c1d9748c5b448c30ce722827dd1ae02527c4ee5
Justas Grinaveckis <grinaveckisjustas@gmail.com>
2019-08-28 22:11:55 +0300

    In progress to splitting long tasks

--- tasks_durations.py
+++ tasks_durations.py
@@ -1,6 +1,6 @@
 import datetime
 
-def adding_tasks_dates(nodeslist: list, date: datetime.datetime):
+def adding_tasks_dates(nodeslist: list, date):
     nodes = [i[0] for i in nodeslist]
     nodes_durations = []
     cur_date = date
@@ -8,17 +8,40 @@ def adding_tasks_dates(nodeslist: list, date: datetime.datetime):
     end = datetime.time(17)
     for node in nodeslist[::-1]:
         dur = node[1]
-        #TODO: tikrinti prasisukima jei didelis uzduoties atlikimo laikas ir dur skaiciuoti
-        if (start <= (cur_date - datetime.timedelta(hours=dur)).time() <= end) & ((cur_date - datetime.timedelta(hours=dur)).days()) == 0:
-            nodes_durations.extend([((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),\
-            cur_date.strftime('%Y-%m-%d %H:%M'))])
-            cur_date = cur_date - datetime.timedelta(hours=dur)
-        elif (start <= (cur_date - datetime.timedelta(hours=dur)).time() <= end) & dur > 8:
-            cur_date = cur_date-datetime.timedelta(days=1)
-            cur_date = cur_date.replace(hour=17)
-            nodes_durations.extend([((cur_date - datetime.timedelta(hours=node[1])).strftime('%Y-%m-%d %H:%M'),\
-            cur_date.strftime('%Y-%m-%d %H:%M'))])
-            cur_date = cur_date - datetime.timedelta(hours=node[1])
+        times_list = []
+        #TODO:sutvarkyti laika 'datetime.datetime' ir 'datetime.time'
+        if (start <= datetime.time((cur_date - datetime.timedelta(hours=dur)).hour) <= end):
+            while dur > 9:
+                start = cur_date.replace(hour=8)
+                finish = cur_date
+                times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+                dur = dur - ((start - finish).hour())
+                cur_date = cur_date-datetime.timedelta(days=1)
+                cur_date = cur_date.replace(hour=17)
+            else:
+                times_list.append(((cur_date - datetime.timedelta(hours=dur)).strftime('%Y-%m-%d %H:%M'),\
+                cur_date.strftime('%Y-%m-%d %H:%M')))
+                nodes_durations.extend(times_list[::-1])
+                cur_date = cur_date - datetime.timedelta(hours=dur)
+        else:
+            while dur > 9:
+                start = cur_date.replace(hour=8)
+                finish = cur_date
+                times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+                dur = dur - ((start - finish).hour())
+                cur_date = cur_date-datetime.timedelta(days=1)
+                cur_date = cur_date.replace(hour=17)
+            else:
+                start = cur_date.replace(hour=8)
+                finish = cur_date
+                times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+                dur = dur - (finish.hour - start.hour)
+                cur_date = cur_date-datetime.timedelta(days=1)
+                cur_date = cur_date.replace(hour=17)
+                times_list.append((((cur_date - datetime.timedelta(hours=dur)).strftime('%Y-%m-%d %H:%M'),\
+                cur_date.strftime('%Y-%m-%d %H:%M'))))
+                nodes_durations.extend(times_list[::-1])
+                cur_date = cur_date - datetime.timedelta(hours=dur)
     #dict kur key - taskas, value - pradzia ir pabaiga
     info_dict = dict(zip(nodes,nodes_durations[::-1]))
     return info_dict
