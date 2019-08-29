6b538a8669c96907ce92bfc2dd3756e54d1eb9d4
jgrinaveckis <grinaveckisjustas@gmail.com>
2019-08-29 15:41:25 +0300

    Added list_appending method to use DRY

--- tasks_durations.py
+++ tasks_durations.py
@@ -16,12 +16,13 @@ def adding_tasks_dates(nodeslist: list, date):
                 cur_date = cur_date-datetime.timedelta(days=1)
                 cur_date = cur_date.replace(hour=17)
             while dur > 9:
-                finish = cur_date
-                start = cur_date.replace(hour=8)
-                times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
-                dur = dur - int(((finish - start).total_seconds())/3600)
-                cur_date = cur_date-datetime.timedelta(days=1)
-                cur_date = cur_date.replace(hour=17)
+                cur_date, times_list, dur = list_appending(times_list, cur_date, dur)
+                # finish = cur_date
+                # start = cur_date.replace(hour=8)
+                # times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+                # dur = dur - int(((finish - start).total_seconds())/3600)
+                # cur_date = cur_date-datetime.timedelta(days=1)
+                # cur_date = cur_date.replace(hour=17)
             else:
                 times_list.append(((cur_date - datetime.timedelta(hours=dur)).strftime('%Y-%m-%d %H:%M'),\
                 cur_date.strftime('%Y-%m-%d %H:%M')))
@@ -32,20 +33,21 @@ def adding_tasks_dates(nodeslist: list, date):
                 cur_date = cur_date-datetime.timedelta(days=1)
                 cur_date = cur_date.replace(hour=17)
             while dur > 9:
-                finish = cur_date
-                start = cur_date.replace(hour=8)
-                times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
-                temp = int(((finish - start).total_seconds())/3600)
-                dur -= temp
-                cur_date = cur_date-datetime.timedelta(days=1)
-                cur_date = cur_date.replace(hour=17)
+                cur_date, times_list, dur = list_appending(times_list, cur_date, dur)
+                # finish = cur_date
+                # start = cur_date.replace(hour=8)
+                # times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+                # dur -= int(((finish - start).total_seconds())/3600)
+                # cur_date = cur_date-datetime.timedelta(days=1)
+                # cur_date = cur_date.replace(hour=17)
             else:
-                finish = cur_date
-                start = cur_date.replace(hour=8)
-                times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
-                dur = dur - int(((finish - start).total_seconds())/3600)
-                cur_date = cur_date-datetime.timedelta(days=1)
-                cur_date = cur_date.replace(hour=17)
+                cur_date, times_list, dur = list_appending(times_list, cur_date, dur)
+                # finish = cur_date
+                # start = cur_date.replace(hour=8)
+                # times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+                # dur = dur - int(((finish - start).total_seconds())/3600)
+                # cur_date = cur_date-datetime.timedelta(days=1)
+                # cur_date = cur_date.replace(hour=17)
                 times_list.append(((cur_date - datetime.timedelta(hours=dur)).strftime('%Y-%m-%d %H:%M'),\
                 cur_date.strftime('%Y-%m-%d %H:%M')))
                 nodes_durations.extend([times_list[::-1]])
@@ -54,11 +56,11 @@ def adding_tasks_dates(nodeslist: list, date):
     info_dict = dict(zip(nodes,nodes_durations[::-1]))
     return info_dict
 
-# def list_appending(times_list:list, cur_date):
-#     start = cur_date.replace(hour=8)
-#     finish = cur_date
-#     times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
-#     dur = dur - ((start - finish).hour())
-#     cur_date = cur_date-datetime.timedelta(days=1)
-#     cur_date = cur_date.replace(hour=17)
-#     return cur_date, times_list
+def list_appending(times_list:list, cur_date, dur):
+    finish = cur_date
+    start = cur_date.replace(hour=8)
+    times_list.append((start.strftime('%Y-%m-%d %H:%M'),finish.strftime('%Y-%m-%d %H:%M')))
+    dur = dur - int(((finish - start).total_seconds())/3600)
+    cur_date = cur_date-datetime.timedelta(days=1)
+    cur_date = cur_date.replace(hour=17)
+    return cur_date, times_list, dur
